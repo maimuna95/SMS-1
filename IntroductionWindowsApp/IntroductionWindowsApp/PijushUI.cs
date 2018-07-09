805ea9a0c05e79a0211103ca7ca1d9d443577ffe
Pijushghosh123 <pijushkanti38@gmail.com>
2018-07-09 17:43:11 -0600

    Success

---
+++ IntroductionWindowsApp/IntroductionWindowsApp/PijushUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroductionWindowsApp
+{
+    public partial class PijushUI : Form
+        string _name=String.Empty;
+        string _email=String.Empty;
+    {
+        public PijushUI()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _name=textBox1.Text;
+            _email=textBox2.Text;
+
+            MessageBox.Show("Success");
+
+
+        }
+    }
+}
